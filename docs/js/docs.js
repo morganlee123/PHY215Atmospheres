92b33a92c1a8fd1b9927d381dcbb8fdbee115df6
Morgan Sandler <sandle20@msu.edu>
2021-03-12 19:09:05 -0500

    add pages

---
+++ docs/js/docs.js
@@ -0,0 +1,9 @@
+var analysisArray = ['planetary-simulations']
+var analysisDict = {'Planetary-Simulations-1': 'planetary-simulations'}
+var analysisArrayMap = {'planetary-simulations': 'Planetary Simulations'}
+var helpArray = []
+var helpDict = {}
+var helpArrayMap = {}
+var aboutArray = []
+var aboutDict = {}
+var aboutArrayMap = {}
