4c8435563fdfab68fd8aea775bf575109dfabee0
Morgan Sandler <sandle20@msu.edu>
2021-03-14 17:13:22 -0400

    adjust content

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,4 +1,4 @@
-var analysisArray = ['planetary-simulations']
+var analysisArray = []
 var analysisDict = {'Planetary-Simulations-1': 'planetary-simulations'}
 var analysisArrayMap = {'planetary-simulations': 'Planetary Simulations'}
 var helpArray = []
