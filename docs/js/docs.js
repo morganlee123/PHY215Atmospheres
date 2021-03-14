ae215d1805950503d380177fd52b1364067f418c
Morgan Sandler <sandle20@msu.edu>
2021-03-14 19:47:14 -0400

    add web files

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,6 +1,6 @@
 var analysisArray = ['weather-forecasting']
-var analysisDict = {'Weather-Forecasting-1': 'weather-forecasting'}
-var analysisArrayMap = {'weather-forecasting': 'Weather Forecasting'}
+var analysisDict = {'The-Future-Physics-of-Weather-Forecasting-1': 'weather-forecasting'}
+var analysisArrayMap = {'weather-forecasting': 'The Future Physics of W ... Forecasting'}
 var aboutArray = []
 var aboutDict = {}
 var aboutArrayMap = {}
