63a2cb9822a11c7ce82462dbc8c101225c9fe795
Morgan Sandler <sandle20@msu.edu>
2021-04-29 14:35:20 -0400

    morgan: Added project 2

---
+++ docs/js/docs.js
@@ -0,0 +1,6 @@
+var analysisArray = ['weather-forecasting', 'habitable']
+var analysisDict = {'The-Future-Physics-of-Weather-Forecasting-1': 'weather-forecasting', 'Thoughts-on-the-habitability-of-newly-discovered-exoplanets-1': 'habitable'}
+var analysisArrayMap = {'climatemodels': 'The Future Physics of W ... Forecasting', 'habitable': 'Thoughts on the habitab ... d exoplanets', 'weather-forecasting': 'The Future Physics of W ... Forecasting'}
+var aboutArray = []
+var aboutDict = {}
+var aboutArrayMap = {}
