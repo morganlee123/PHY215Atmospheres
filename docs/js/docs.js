f79759b3f2806da82d907d776a342199a9297833
Morgan Sandler <sandle20@msu.edu>
2021-04-29 14:34:11 -0400

    morgan: Added project 2

--- docs/js/docs.js
+++
@@ -1,6 +0,0 @@
-var analysisArray = ['weather-forecasting']
-var analysisDict = {'The-Future-Physics-of-Weather-Forecasting-1': 'weather-forecasting'}
-var analysisArrayMap = {'weather-forecasting': 'The Future Physics of W ... Forecasting'}
-var aboutArray = []
-var aboutDict = {}
-var aboutArrayMap = {}
