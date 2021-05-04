6079d0fd69b883c77e5f6c3b1bf6b6694774ce4b
Morgan Sandler <sandle20@msu.edu>
2021-05-04 17:46:00 -0400

    init

---
+++ docs/js/docs.js
@@ -0,0 +1,6 @@
+var analysisArray = ['weather-forecasting', 'habitable', 'honors']
+var analysisDict = {'The-Future-Physics-of-Weather-Forecasting-1': 'weather-forecasting', 'Thoughts-on-the-habitability-of-newly-discovered-exoplanets-1': 'habitable', 'Computational-Fluid-Dynamics-to-study-social-distancing-recommendations-by-the-CDC-1': 'honors'}
+var analysisArrayMap = {'climatemodels': 'The Future Physics of W ... Forecasting', 'habitable': 'Thoughts on the habitab ... d exoplanets', 'honors': 'Computational Fluid Dyn ... s by the CDC', 'weather-forecasting': 'The Future Physics of W ... Forecasting'}
+var aboutArray = []
+var aboutDict = {}
+var aboutArrayMap = {}
